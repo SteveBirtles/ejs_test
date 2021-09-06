d9a71470835896f5f5f5b7f6adf7c6fcb267ac87
Steve Birtles <sbirtles@farnborough.ac.uk>
2021-09-06 22:07:28 +0100

    Ready

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const express = require("express");
+const moment = require("moment");
+
 const app = express();
+
+//app.use(express.json()) // for parsing application/json
+//app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
 
-    const response = await fetch('https://www.gov.uk/bank-holidays.json');
-    const data = await response.json();
+  console.log(req.method, "request:", req.hostname, req.path);
+
+  const response = await fetch('https://www.gov.uk/bank-holidays.json');
+  const data = await response.json();
+
+  if (!data.hasOwnProperty("england-and-wales") ||
+    !data["england-and-wales"].hasOwnProperty("events")) {
+    res.status(500);
+    res.data("Error");
+    return;
+  }
+
+  const allHolidays = data["england-and-wales"]["events"];
+
+  const holidaysWithDaysToGo = allHolidays.map(holiday => {
     
-    if (!data.hasOwnProperty("england-and-wales") ||
-        !data["england-and-wales"].hasOwnProperty("events")) {
-            res.status(500);
-            res.data("Error");
-            return;
-        }
+    const now = moment();    
+    const then = moment(holiday.date);
+    
+    return {
+      title: holiday.title,
+      date: holiday.date,
+      daysToGo: then.diff(now, "days"),
+      substitueDay: holiday.notes.includes("Substitute day"),
+      buntingRequired: holiday.bunting
+    }
 
-        const holidays = data["england-and-wales"]["events"];
+  });
 
-    res.render("index", { holidays });
+  const futureHolidays = holidaysWithDaysToGo.filter(
+    holiday => holiday.daysToGo >= 0
+  );
+
+  res.render("index", { holidays: futureHolidays });
 })
 
 app.listen(3000);
